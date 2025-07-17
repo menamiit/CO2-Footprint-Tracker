7e53f8dac569f957e713f9b565ff24f7b1a9c78e
Namit <pareeknamit8@gmail.com>
2025-07-17 18:03:47 +0530

    Developments in the backend and a few other things

---
+++ server/controllers/authController.js
@@ -0,0 +1,51 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPass,
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: "User registered successfully" });
+
+  } catch (err) {
+    res.status(500).json({ message: "User registration failed", error: err.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    if (!user)
+      return res.status(404).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign(
+        {id:user._id, username:user.username},
+        process.env.JWT_SECRET,
+        {expiresIn: '1h'}
+    );
+
+    res.status(200).json({ token, username: user.username });
+
+  } catch (error) {
+    res.status(500).json({ message: "Login failed", error: error.message });
+  }
+};
+
+module.exports = { registerUser, loginUser };
